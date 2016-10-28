a055172d93e1ef1978f07388004fcfa3afe6e304
Alexander <greyalex2003@yahoo.com>
2016-10-29 00:19:49 +0300

    GamEn sample with data theoretically complete

--- julia/Sample.jl
+++ julia/Sample.jl
@@ -1,47 +1,92 @@
 module Sample
 
 import GameEn
+import SimpleCam
 
-function onviewport(engine::GamEn.Engine, event::Symbol, width::Integer, height::Integer)
+function onviewport(engine::GamEn.Engine, event::Symbol, width::Integer, height::Integer, font::GRU.Font)
 	m = Math3D.persp_horizontal_fov(pi/2, width / height, 0.1f0, 100, leftHanded = true)
 	GRU.setproj(engine.renderer.camera, m)
 	GRU.settransform(engine.renderer.camera, Math3D.trans(Float32[0, 0, -1]))
 
-	global font
 	m = Math3D.ortho(0, width, 0, height, -1.0f0, 1.0f0)
 	GRU.setuniform(font.model.material, :projection, m)
 end
 
-function onupdate(engine::GamEn.Engine, event::Symbol)
+function onrender(engine::Engine, event::Symbol, objects::Dict{Symbol, Any})
+	for (k, v) in objects
+		GRU.add(engine.renderer, v)
+	end
 end
 
-function oninput(engine::GamEn.Engine, event::Symbol)
+function onupdate(engine::GamEn.Engine, event::Symbol, objects::Dict{Symbol, Any})
+	deltaTime = engine.timeNow - engine.timePrev
+	sphere = objects[:sphere]
+	currentModel = GRU.gettransform(sphere)
+	rotMatrix = Math3D.rotz(eye(Float32, 4), Float32(deltaTime * pi / 2))
+	newModel = rotMatrix * currentModel
+	GRU.settransform(sphere, newModel)
+	airplane = objects[:airplane]
+	currentModel = GRU.gettransform(airplane)
+	GRU.settransform(airplane, currentModel * rotMatrix)
+
+	font = objects[:font]
+	GRU.cleartext(font)
+	cursor = FTFont.TextCursor(font.font.size.x, font.font.lineDistance)
+	frameCount = objects[:frames]
+	frameTimes = objects[:frameTimes]
+	frames = length(frameTimes)
+	curFrame = (frameCount-1)%frames+1
+	nextFrame = frameCount%frames+1
+	fps = frames / (frameTimes[curFrame] - frameTimes[nextFrame])
+	frameTimes[nextFrame] = engine.timeNow
+	objects[:frames] = frameCount + 1
+	GRU.drawtext(font, cursor, "FPS: " * string(round(fps, 2)), (1f0, 1f0, 0f0, 1f0))
+end
+
+function oninput(engine::GamEn.Engine, event::Symbol, freeCam::SimpleCam.FreeCamera)
+	SimpleCam.process_input(freeCam)
 end
 
 function setup_matrices(model::GRU.Model)
-    GRU.setuniform(model.material, :model, model.transform)
-    camera = model.material.shader.renderer.camera
-    GRU.setuniform(model.material, :view, GRU.getview(camera))
-    GRU.setuniform(model.material, :projection, GRU.getproj(camera))
-    if GRU.hasuniform(model.material, :modelIT)
-        GRU.setuniform(model.material, :modelIT, transpose(inv(model.transform[1:3, 1:3])))
-    end
+	GRU.setuniform(model.material, :model, model.transform)
+	camera = model.material.shader.renderer.camera
+	GRU.setuniform(model.material, :view, GRU.getview(camera))
+	GRU.setuniform(model.material, :projection, GRU.getproj(camera))
+	if GRU.hasuniform(model.material, :modelIT)
+		GRU.setuniform(model.material, :modelIT, transpose(inv(model.transform[1:3, 1:3])))
+	end
 end
 
 function load_defs(engine::GamEn.Engine)
-	GamEn.load_def(engine, "")
-
+	loaded = Dict{Symbol, Any}()
+	loaded[:font] = GamEn.load_def(engine, "roboto")
+	loaded[:triangle] = GamEn.load_def(engine, "model_triangle")
+	loaded[:sphere] = GamEn.load_def(engine, "model_sphere")
+	loaded[:airplane] = GamEn.load_def(engine, "cessna")
 end
 
 function run()
 	engine = GamEn.Engine("data")
 	GamEn.init(engine)
 
-	GamEn.add_event(engine, :viewport, onviewport)
-	GamEn.add_event(engine, :update, onupdate)
-	GamEn.add_event(engine, :input, oninput)
+	freeCam = SimpleCam.FreeCamera(engine.renderer, engine.window, Float32[2, 2, 2], Float32[pi/2, pi/2, pi/2])
 
-	load_defs(engine)
+	loaded = load_defs(engine)
+	loaded[:frameTimes] = fill(0.0, 10)
+	loaded[:frames] = 1
+
+	GamEn.add_event(engine, :viewport) do engine, event, width, height
+		onviewport(engine, event, width, height, loaded[:font])
+	end
+	GamEn.add_event(engine, :render) do engine, event
+		onrender(engine, event, loaded)
+	end
+	GamEn.add_event(engine, :update) do engine, event
+		onupdate(engine, event, loaded)
+	end
+	GamEn.add_event(engine, :input) do engine, event
+		oninput(engine, event, freeCam)
+	end
 
 	GamEn.run(engine)
 
