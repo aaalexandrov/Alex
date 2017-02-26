6260179830ab013d161fa9257aac90b856348539
Alexander <greyalex2003@yahoo.com>
2017-02-26 23:38:29 +0200

    Task interface cleanup

--- Task/Task/Task.cpp
+++ Task/Task/Task.cpp
@@ -17,27 +17,32 @@ extern "C" {
   void swapcontext(Task *from, Task *to);
 }
 
-void create_task(Task *task, Task *parent, size_t stack_size, taskfn fn, void *user)
+void init_task(Task *task, Task *parent, void *stack_base, size_t stack_size, taskfn fn, void *user)
 {
   task->user = user;
   task->stack_ptr = 0;
   task->parent = parent;
-  task->stack_base = stack_size > 0 ? malloc(stack_size) : 0;
+  task->stack_base = stack_base;
   task->stack_size = stack_size;
 
-  // stack_size == 0 means currently executing stack, so no need to initialize anything there
-  if (task->stack_size > 0)
+  // stack_base == 0 means currently executing stack, so no need to initialize anything there
+  if (task->stack_base)
     makecontext(task, fn);
-}
-
-void delete_task(Task *task)
-{
-  free(task->stack_base);
 }
 
 bool hasended_task(Task *task)
 {
   return !task->parent && task->stack_base;
+}
+
+void create_task(Task *task, Task *parent, size_t stack_size, taskfn fn, void *user)
+{
+  init_task(task, parent, stack_size > 0 ? malloc(stack_size) : 0, stack_size, fn, user);
+}
+
+void delete_task(Task *task)
+{
+  free(task->stack_base);
 }
 
 void fib(Task *task)
