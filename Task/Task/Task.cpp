894b273220fb3ed70ab69f33539562a6a72d9e02
Alexander <greyalex2003@yahoo.com>
2017-02-26 19:14:20 +0200

    Prototype of makecontext / swapcontext for Windows.

---
+++ Task/Task/Task.cpp
@@ -0,0 +1,94 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+struct Task {
+  void   *user;
+  void   *stack_ptr;
+  Task   *parent;
+  void   *stack_base;
+  size_t  stack_size;
+};
+
+typedef void(*taskfn)(Task *task);
+
+extern "C" {
+  void makecontext(Task *task, taskfn fn);
+  void swapcontext(Task *from, Task *to);
+}
+
+void create_task(Task *task, Task *parent, size_t stack_size, taskfn fn, void *user)
+{
+  task->user = user;
+  task->stack_ptr = 0;
+  task->parent = parent;
+  task->stack_base = stack_size > 0 ? malloc(stack_size) : 0;
+  task->stack_size = stack_size;
+
+  // stack_size == 0 means currently executing stack, so no need to initialize anything there
+  if (task->stack_size > 0)
+    makecontext(task, fn);
+}
+
+void delete_task(Task *task)
+{
+  free(task->stack_base);
+}
+
+bool hasended_task(Task *task)
+{
+  return !task->parent && task->stack_base;
+}
+
+void fib(Task *task)
+{
+  int a = 0, b = 1;
+  while (a < 100)
+  {
+    *(int*)(task->user) = a;
+    swapcontext(task, task->parent);
+    int t = a;
+    a = b;
+    b = t + b;
+  }
+}
+
+void task1(Task *task)
+{
+  Task fib_task;
+  int param;
+  std::cout << "Task 1" << std::endl;
+  create_task(&fib_task, task, 16384, fib, &param);
+
+  while (true)
+  {
+    swapcontext(task, &fib_task);
+    if (hasended_task(&fib_task))
+      break;
+    std::cout << param << std::endl;
+  }
+
+  delete_task(&fib_task);
+  std::cout << "Task 1 signing off" << std::endl;
+}
+
+int main()
+{
+  Task main_task, other1;
+  create_task(&main_task, 0, 0, 0, 0);
+  create_task(&other1, &main_task, 16384, task1, 0);
+
+  std::cout << "Starting" << std::endl;
+
+  swapcontext(&main_task, &other1);
+
+  std::cout << "Ending" << std::endl;
+
+  delete_task(&other1);
+  delete_task(&main_task);
+
+  std::cin.ignore();
+
+  return 0;
+}
+
---
+++ Task/Task/context.asm
@@ -0,0 +1,142 @@
+.code
+
+Task STRUCT
+	user		QWORD	?
+	stack_ptr	QWORD	?
+	parent		QWORD	?
+	stack_base	QWORD	?
+	stack_size	QWORD	?
+Task ENDS
+
+; (SIZEOF Saved) % 16 == 8 because of stack alignment
+Saved STRUCT
+	_xmm6	XMMWORD	?
+	_xmm7	XMMWORD	?
+	_xmm8	XMMWORD	?
+	_xmm9	XMMWORD	?
+	_xmm10	XMMWORD	?
+	_xmm11	XMMWORD	?
+	_xmm12	XMMWORD	?
+	_xmm13	XMMWORD	?
+	_xmm14	XMMWORD	?
+	_xmm15	XMMWORD	?
+	_mxcsr	DWORD	?
+	_x87cw	DWORD	?
+	_r12	QWORD	?
+	_r13	QWORD	?
+	_r14	QWORD	?
+	_r15	QWORD	?
+	_rsi	QWORD	?
+	_rdi	QWORD	?
+	_rbx	QWORD	?
+	_rbp	QWORD	?
+Saved ENDS
+
+makecontext PROC FRAME
+	.endprolog
+
+	;RCX contains the task to initialize
+	;RDX contains the function pointer to execute when the task is switched to
+
+	;align stack
+	mov		RAX, [RCX].Task.stack_base
+	add		RAX, [RCX].Task.stack_size
+	and		RAX, -16
+
+	;allocate stack for Saved structure + 8 bytes for the trampoline address
+	sub		RAX, 8 + SIZEOF Saved
+
+	;initialize the Saved structure at the top of the stack
+	;set MMX/SSE and x87 control word fields the same as the creator
+	stmxcsr	[RAX].Saved._mxcsr
+	fnstcw	WORD PTR [RAX].Saved._x87cw
+	;set only RSI and RDI as they will be used in the trampoline, we don't care about the rest
+	mov		[RAX].Saved._rsi, RCX
+	mov		[RAX].Saved._rdi, RDX
+
+	;push trampoline address at the bottom of the stack
+	lea		R9, trampoline
+	mov		[RAX + SIZEOF Saved], R9
+
+	;save the stack pointer for the new task
+	mov		[RCX].Task.stack_ptr, RAX
+
+	ret
+
+trampoline:
+	;set task argument and call the task entry point
+	mov		RCX, RSI
+	;alloc space for argument registers as required by x64 ABI
+	sub		RSP, 64
+	call	RDI
+	add		RSP, 64
+	;set arguments and call swapcontext to return to the parent task
+	mov		RCX, RSI
+	mov		RDX, [RSI].Task.parent
+	;zero out the task parent to indicate the task's finished
+	xor		RAX, RAX
+	mov		[RSI].Task.parent, RAX
+	call	swapcontext
+	;we should not return to a completed task
+	int		3
+
+makecontext ENDP
+
+swapcontext PROC FRAME
+	.endprolog
+
+	;RCX contains the FROM task
+	;RDX contains the TO task
+
+	sub		RSP, SIZEOF Saved
+	movaps	[RSP].Saved._xmm6, XMM6
+	movaps	[RSP].Saved._xmm7, XMM7
+	movaps	[RSP].Saved._xmm8, XMM8
+	movaps	[RSP].Saved._xmm9, XMM9
+	movaps	[RSP].Saved._xmm10, XMM10
+	movaps	[RSP].Saved._xmm11, XMM11
+	movaps	[RSP].Saved._xmm12, XMM12
+	movaps	[RSP].Saved._xmm13, XMM13
+	movaps	[RSP].Saved._xmm14, XMM14
+	movaps	[RSP].Saved._xmm15, XMM15
+	stmxcsr	[RSP].Saved._mxcsr
+	fnstcw	WORD PTR [RSP].Saved._x87cw
+	mov		[RSP].Saved._r12, R12
+	mov		[RSP].Saved._r13, R13
+	mov		[RSP].Saved._r14, R14
+	mov		[RSP].Saved._r15, R15
+	mov		[RSP].Saved._rsi, RSI
+	mov		[RSP].Saved._rdi, RDI
+	mov		[RSP].Saved._rbx, RBX
+	mov		[RSP].Saved._rbp, RBP
+
+	mov		[RCX].Task.stack_ptr, RSP
+	mov		RSP, [RDX].Task.stack_ptr
+
+	movaps	XMM6, [RSP].Saved._xmm6 
+	movaps	XMM7, [RSP].Saved._xmm7 
+	movaps	XMM8, [RSP].Saved._xmm8 
+	movaps	XMM9, [RSP].Saved._xmm9 
+	movaps	XMM10, [RSP].Saved._xmm10
+	movaps	XMM11, [RSP].Saved._xmm11
+	movaps	XMM12, [RSP].Saved._xmm12
+	movaps	XMM13, [RSP].Saved._xmm13
+	movaps	XMM14, [RSP].Saved._xmm14
+	movaps	XMM15, [RSP].Saved._xmm15
+	ldmxcsr	[RSP].Saved._mxcsr
+	fldcw	WORD PTR [RSP].Saved._x87cw
+	mov		R12, [RSP].Saved._r12
+	mov		R13, [RSP].Saved._r13
+	mov		R14, [RSP].Saved._r14
+	mov		R15, [RSP].Saved._r15
+	mov		RSI, [RSP].Saved._rsi
+	mov		RDI, [RSP].Saved._rdi
+	mov		RBX, [RSP].Saved._rbx
+	mov		RBP, [RSP].Saved._rbp
+	add		RSP, SIZEOF Saved
+
+	ret
+
+swapcontext ENDP
+
+END
