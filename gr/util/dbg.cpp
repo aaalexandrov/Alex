ba74efcb053a7a91c3971c22cec2c74dace16796
Alexander <greyalex2003@yahoo.com>
2020-06-21 13:14:34 +0300

    Add simple Assert, able to break in the debugger or abort the program on Windows

--- gr/util/dbg.cpp
+++ gr/util/dbg.cpp
@@ -1,5 +1,28 @@
 #include "dbg.h"
+#include <stdio.h>
 
 NAMESPACE_BEGIN(util)
 
+void AssertFailed(char const *message, char const *file, unsigned line)
+{
+	char msg[1024];
+	sprintf_s(msg, ArraySize(msg), "%s(%u): Assert failed: %s", file, line, message);
+	LogLine(std::cerr, msg);
+
+#if defined(_WIN32)
+	int res = MessageBoxA(nullptr, msg, "Assert failed", MB_ABORTRETRYIGNORE | MB_DEFBUTTON2 | MB_ICONEXCLAMATION | MB_TASKMODAL | MB_SETFOREGROUND);
+	switch (res) {
+		case IDABORT:
+			std::abort();
+			break;
+		case IDRETRY:
+			DebugBreak();
+			break;
+		case IDIGNORE:
+			break;
+	}
+#endif
+}
+
 NAMESPACE_END(util)
+
--- gr/util/dbg.h
+++ gr/util/dbg.h
@@ -5,13 +5,20 @@
 
 NAMESPACE_BEGIN(util)
 
-#define ASSERT assert
-
-#if !defined(NDEBUG)
-#define LOG(...) util::LogLine(std::cerr, __VA_ARGS__)
+#if defined(NDEBUG)
+#define ASSERT(expression) ((void)0)
 #else
-#define LOG(...)
+#define ASSERT(expression) (void) ((!!(expression)) || (util::AssertFailed(#expression, __FILE__, (unsigned)__LINE__), 0))
 #endif
+
+void AssertFailed(char const *message, char const *file, unsigned line);
+
+#if defined(NDEBUG)
+#define LOG(...) ((void)0)
+#else
+#define LOG(...) util::LogLine(std::cerr, __VA_ARGS__)
+#endif
+
 
 template <typename ARG, typename... ARGS>
 void LogLine(std::ostream &out, ARG&& arg, ARGS&&... args) 
